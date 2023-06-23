8a463513cc3cfcc09a9e900783cac1a8607a872f
«Irina <pozhelukhina@yandex.ru>
2023-06-23 19:47:53 +0300

    task62

---
+++ task62/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая заполнит спирально массив N на N.
+// N вводит с клавиатуры пользователь.
+// Например, N=4 и на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],4} ");
+        System.Console.WriteLine();
+    }
+}
+
+void FillArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n,n];
+FillArraySpiral(array, n);
+PrintArray(array);
